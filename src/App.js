ca73676930b0d50240b5bc7dd5cb483a706fd188
Aman Sharma <aman.sharma@techiebutler.com>
2023-10-30 22:10:10 +0530

    Build

--- src/App.js
+++ src/App.js
@@ -12,6 +12,28 @@ import GoogleTranslate from './Components/GoogleTranslate/GoogleTranslate';
 
 // Parent component
 function App() {
+    const [code, setCode] = React.useState('');
+    const [allow, setAllow] = React.useState('')
+
+    React.useEffect(() => {
+        let code = localStorage.getItem('code');
+        if(code)
+            setAllow(code)
+    })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        // Check if the entered code is a 4-digit numeric value
+        if (/^\d{4}$/.test(code)) {
+            // Update the state or perform any other action
+            setAllow(code)
+            localStorage.setItem('code', code)
+            console.log('Code submitted:', code);
+        } else {
+            console.log('Invalid code. Please enter a 4-digit numeric code.');
+        }
+    };
+
 
     const toast = () => {
         return (
@@ -30,23 +52,39 @@ function App() {
             />
         );
     };
-
-
     return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<Home />} />
+        <div>
+            {allow !== "8741" ?
+                <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+                    <form onSubmit={handleSubmit}>
+                        <label>
+                            Enter 4-digit Code:
+                            <input
+                                type="text"
+                                value={code}
+                                maxLength={4}
+                                onChange={(e) => setCode(e.target.value.replace(/\D/, ''))}
+                            />
+                        </label>
+                        <button type="submit">Submit</button>
+                    </form>
+                </div> :
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<Home />} />
 
-                <Route path="/about" element={About} />
-                <Route path="/contact" element={Contact} />
-                <Route path="/admin" element={<Admin />} />
-                <Route path="/translate" element={<GoogleTranslate />} />
-                <Route path="/testing" element={<Testing />} />
+                        <Route path="/about" element={About} />
+                        <Route path="/contact" element={Contact} />
+                        <Route path="/admin" element={<Admin />} />
+                        <Route path="/translate" element={<GoogleTranslate />} />
+                        <Route path="/testing" element={<Testing />} />
 
-                <Route element={NotFound} />
-            </Routes>
-            {toast()}
-        </Router>
+                        <Route element={NotFound} />
+                    </Routes>
+                    {toast()}
+                </Router>}
+        </div>
+
     );
 }
 
